5f4198f99d7131cb671ad5f0d234e3abf60cf507
1-arua <64558593+1-arua@users.noreply.github.com>
2024-07-11 22:45:49 +0900

    tailwindcss と daisyUI で tauri の初期アプリのレイアウトを再現する
    ロゴ要素は似たスタイルを当てるため、ループで処理する

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,63 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+	const imgs = [
+		{ href: "https://vitejs.dev", src: "/vite.svg", alt: "Vite logo" },
+		{ href: "https://tauri.app", src: "/tauri.svg", alt: "Tauri logo" },
+		{ href: "https://reactjs.org", src: reactLogo, alt: "React logo" },
+	];
+	const [greetMsg, setGreetMsg] = useState("");
+	const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		setGreetMsg(await invoke("greet", { name }));
+	}
 
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+	return (
+		<>
+			<div className="mx-auto mt-10 max-w-xl text-center">
+				<h1 className="text-4xl">Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+				<div className="join">
+					{imgs.map((img) => (
+						<div className="avatar join-item h-28 rounded" key={img.src}>
+							<a href={img.href}>
+								<img src={img.src} alt={img.alt} className="p-2" />
+							</a>
+						</div>
+					))}
+				</div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+				<p className="text-center">
+					Click on the Tauri, Vite, and React logos to learn more.
+				</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
+				<form
+					className="flex justify-center mt-10"
+					onSubmit={(e) => {
+						e.preventDefault();
+						greet();
+					}}
+				>
+					<input
+						id="greet-input"
+						onChange={(e) => setName(e.currentTarget.value)}
+						placeholder="Enter a name..."
+						className="border-2 border-gray-300 p-2 rounded-md"
+					/>
+					<button
+						type="submit"
+						className="bg-blue-500 text-white rounded-md p-2 ml-2 hover:bg-blue-700"
+					>
+						Greet
+					</button>
+				</form>
 
-      <p>{greetMsg}</p>
-    </div>
-  );
+				<p>{greetMsg}</p>
+			</div>
+		</>
+	);
 }
 
 export default App;
